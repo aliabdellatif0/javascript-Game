e2ea2fb34c5803b18f2c343fa369de2a44fdd169
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-09-27 22:43:18 +0100

    multiple tubes have been generated

--- main.js
+++ main.js
@@ -27,12 +27,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function generateTube(){
         let tubeLeft = 500
-        let tubeBottom = 150
+        let randomRange = Math.random() * 60
+        let tubeBottom = randomRange
         const tube = document.createElement('div')
         tube.classList.add('tube')
         gameWindow.appendChild(tube)
         tube.style.left = tubeLeft + 'px'
         tube.style.bottom = tubeBottom + 'px'
+
+        function moveTube() {
+            tubeLeft = tubeLeft - 2
+            tube.style.left = tubeLeft + 'px'
+
+            if(tubeLeft === -60){
+                clearInterval(tubeTimer)
+                gameWindow.removeChild(tube)
+            }
+        }
+        let tubeTimer = setInterval(moveTube, 20)
+        setTimeout(generateTube, 3000)
     }
     generateTube()
 })
